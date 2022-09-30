e88884659b2335c37ad81a7487d4ab33253aee02
Ben Hope <ben.hope@awaze.co.uk>
2022-09-30 18:50:29 +0100

    project files

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val kotestVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,10 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+
+    @Test
+    fun `my first test`() {
+        1 shouldBe 1
+    }
+}

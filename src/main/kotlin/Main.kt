3934c091a5c439349ba3e0c24d9af05cb06d8006
Ben Hope <benhope.itc@gmail.com>
2023-11-03 23:14:02 +0000

    Test pre-commit hook 7, spotlessApply auto format

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,6 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    println(BasicCodeCoverage().HelloWorld())
+    println(BasicCodeCoverage().helloWorld())
     println(BasicCodeCoverage().helloWorld2())
 }

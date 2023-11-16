9d33a9ff85a55aaaa97bf9ce1273127348ccbad7
Ben Hope <benhope.itc@gmail.com>
2023-11-16 19:52:56 +0000

    Updated pre-commit hook to run tests and spotlessApply.

--- src/main/kotlin/com/test/coverage/AdvancedCodeCoverage.kt
+++ src/main/kotlin/com/test/coverage/AdvancedCodeCoverage.kt
@@ -1,7 +1,7 @@
 package com.test.coverage
 
 class AdvancedCodeCoverage() {
-    fun helloWorld() = "Hello 2 advanced ${world()}!"
+    fun HelloWorld() = "Hello 2 advanced ${world()}!"
 
     fun helloWorld2(): String {
         return "Hello advanced ${world()}!"
